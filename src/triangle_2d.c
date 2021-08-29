2a00ff211a2358162ad3f2ea6e197a5f22145e13
LogicEu <eugenioarteagaa@gmail.com>
2021-08-28 23:56:22 -0400

    add parenthesis to bool and expression in tiangle_2d.c

--- src/triangle_2d.c
+++ src/triangle_2d.c
@@ -12,8 +12,8 @@ bool tri2D_point_overlap(const Tri2D* restrict tri, vec2 p)
     float x = p.x - tri->a.x;
     float y = p.y - tri->a.y;
     bool ab = (tri->b.x - tri->a.x) * y - (tri->b.y - tri->a.y) * x > 0;
-    if ((tri->c.x - tri->a.x) * y - (tri->c.y - tri->a.y) * x > 0.0 == ab) return false;
-    if ((tri->c.x - tri->b.x) * (p.y - tri->b.y) - (tri->c.y - tri->b.y) * (p.x - tri->b.x) > 0.0 != ab) return false;
+    if (((tri->c.x - tri->a.x) * y - (tri->c.y - tri->a.y) * x > 0.0) == ab) return false;
+    if (((tri->c.x - tri->b.x) * (p.y - tri->b.y) - (tri->c.y - tri->b.y) * (p.x - tri->b.x) > 0.0) != ab) return false;
     return true;
 }
 
